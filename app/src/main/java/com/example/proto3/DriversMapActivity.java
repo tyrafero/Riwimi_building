fbf1ffa8ac037eb3ace4e1bce3c68fa5eaed5449
Tyrafero <ashutoshsinghkhadka@gmail.com>
2020-01-30 12:39:19 +0545

    Location Problem Solved
    Signed-off-by: Tyrafero <ashutoshsinghkhadka@gmail.com>

--- app/src/main/java/com/example/proto3/DriversMapActivity.java
+++ app/src/main/java/com/example/proto3/DriversMapActivity.java
@@ -14,11 +14,14 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.LocationCallback;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -31,6 +34,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -46,7 +50,11 @@ public class DriversMapActivity extends FragmentActivity implements OnMapReadyCa
     private Button mLogout;
     private boolean isloggingout;
 
-    @Override
+
+
+            //Oncreate function
+
+             @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drivers_map);
@@ -58,7 +66,7 @@ public class DriversMapActivity extends FragmentActivity implements OnMapReadyCa
 
         mapFragment.getMapAsync(this);
 
-        mLogout = (Button) findViewById(R.id.DriverLogoutBtn);
+        mLogout = (Button) findViewById(R.id.DriverLogoutBtn); //Driver logout activity
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,31 +81,31 @@ public class DriversMapActivity extends FragmentActivity implements OnMapReadyCa
         });
     }
 
-             private void checkLocationPermission()
-             {
-                 if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                private void checkLocationPermission()  //after google banned the direct use of its api
                  {
-                     if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)) {
-                         new AlertDialog.Builder(this)
-                                 .setTitle("Giver permission")
-                                 .setMessage("Give permission")
-                                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                                     @Override
-                                     public void onClick(DialogInterface dialog, int which) {
-                                         ActivityCompat.requestPermissions(DriversMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                                     }
-                                 })
-                                 .create()
-                                 .show();
-                     }
-                     else{
-                         ActivityCompat.requestPermissions(DriversMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                     if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                     {
+                         if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)) {
+                             new AlertDialog.Builder(this)
+                                     .setTitle("Giver permission")
+                                     .setMessage("Give permission")
+                                     .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                         @Override
+                                         public void onClick(DialogInterface dialog, int which) {
+                                             ActivityCompat.requestPermissions(DriversMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                                         }
+                                     })
+                                     .create()
+                                     .show();
+                         }
+                         else{
+                             ActivityCompat.requestPermissions(DriversMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                         }
                      }
                  }
-             }
 
 
-    @Override
+             @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         locationRequest = new LocationRequest();
@@ -106,32 +114,26 @@ public class DriversMapActivity extends FragmentActivity implements OnMapReadyCa
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); //best location<also drains a lot of battery>
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
+                mMap.setMyLocationEnabled(true);
             } else {
                 checkLocationPermission();
             }
         }
 
 
-       // @Override
-        //public void onLocationChanged (Location location)
+
+
+
+
+
+
+
+
+
+
+        //private void disconnectdriver ()
         //{
-          //  lastLocation = location;
-           // LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
-            //mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
-            //mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
-
-            //String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-            //DatabaseReference DriverAvailabilityRef = FirebaseDatabase.getInstance().getReference().child("Drivers Available");
-
-            //GeoFire geoFire = new GeoFire(DriverAvailabilityRef);
-            //geoFire.setLocation(userID, new GeoLocation(location.getLatitude(), location.getLongitude()));
-
-        //}
-
-       // private void disconnectdriver ()
-        //{
-          //  super.onStop();
+            //super.onStop();
             //String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
             //DatabaseReference DriverAvailabilityRef = FirebaseDatabase.getInstance().getReference().child("Drivers Available");
 
