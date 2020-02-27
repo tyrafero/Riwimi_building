e06fbdb921c72f9821ec68640cb01a9342649fa6
nilesh-shrestha <52340782+nilesh-shrestha@users.noreply.github.com>
2020-02-27 18:12:26 +0545

    neww

--- app/src/main/java/com/example/proto3/DrivAccActivity.java
+++ app/src/main/java/com/example/proto3/DrivAccActivity.java
@@ -1,15 +1,44 @@
 package com.example.proto3;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class DrivAccActivity extends AppCompatActivity {
+
+    private boolean islogout;
+    private Button mLogout;
+    private TextView Name,Address,Email,Phone;
+    private ImageView Image;
+    private FirebaseDatabase database;
+    private DatabaseReference userRef;
+    private static final String USER = "user";
+    String email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +46,7 @@ public class DrivAccActivity extends AppCompatActivity {
         setContentView(R.layout.activity_driv_acc);
 
 
+        //Bottom Navigation Part
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.setSelectedItemId(R.id.nav_account);
@@ -30,18 +60,45 @@ public class DrivAccActivity extends AppCompatActivity {
                     case R.id.nav_menu:
                         startActivity(new Intent(getApplicationContext()
                                 , DrivMenuActivity.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.nav_home:
                         startActivity(new Intent(getApplicationContext()
-                                ,DriversMapActivity.class));
-                        overridePendingTransition(0,0);
+                                , DriversMapActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
             }
         });
 
+        //Driver logout activity
+        mLogout = (Button) findViewById(R.id.logout);
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                islogout = true;
+
+                disconnectDriver();
+                LogOutUser();
+            }
+        });
+    }
+
+
+    private void disconnectDriver() {
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("driversAvailable");
+        GeoFire geoFire = new GeoFire(ref);
+        geoFire.removeLocation(userId);
+    }
+
+    public void LogOutUser()
+    {
+        Intent startPageIntent = new Intent(DrivAccActivity.this , WelcomeActivity.class);
+        startPageIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(startPageIntent);
+        finish();
     }
 
 }
--- app/src/main/java/com/example/proto3/DriversMapActivity.java
+++ app/src/main/java/com/example/proto3/DriversMapActivity.java
@@ -9,8 +9,11 @@ import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import android.os.Looper;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
@@ -19,8 +22,9 @@ import androidx.fragment.app.FragmentActivity;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
-import com.google.android.gms.location.FusedLocationProviderClient;
+//import com.firebase.geofire.LocationCallback;
 import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
@@ -34,61 +38,59 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-//import com.firebase.geofire.LocationCallback;
 
-public class DriversMapActivity extends FragmentActivity implements OnMapReadyCallback
-         {
+public class DriversMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     Location lastLocation;
     LocationRequest locationRequest;
 
+
     private FusedLocationProviderClient mFusedLocationClient;
 
-    private Button mLogout;
-    private boolean isloggingout;
     private String customerId = "";
     private float rideDistance;
 
+    private Switch mWorkingSwitch;
 
 
 
-             //Oncreate function
+    //Oncreate function
 
-             @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drivers_map);
 
-         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-         bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
 
-         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-             @Override
-             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                 switch (menuItem.getItemId()) {
-                     case R.id.nav_account:
-                         startActivity(new Intent(getApplicationContext()
-                                 , DrivAccActivity.class));
-                         overridePendingTransition(0,0);
-                         return true;
-                     case R.id.nav_menu:
-                         startActivity(new Intent(getApplicationContext()
-                                 , DrivMenuActivity.class));
-                         overridePendingTransition(0,0);
-                         return true;
-                     case R.id.nav_home:
-                         return true;
-                 }
-                 return false;
-             }
-         });
-
-
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_account:
+                        startActivity(new Intent(getApplicationContext()
+                                , DrivAccActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.nav_menu:
+                        startActivity(new Intent(getApplicationContext()
+                                , DrivMenuActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.nav_home:
+                        return true;
+                }
+                return false;
+            }
+        });
 
 
-                 // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
@@ -96,125 +98,133 @@ public class DriversMapActivity extends FragmentActivity implements OnMapReadyCa
 
         mapFragment.getMapAsync(this);
 
-        mLogout = (Button) findViewById(R.id.DriverLogoutBtn); //Driver logout activity
-        mLogout.setOnClickListener(new View.OnClickListener() {
+        mWorkingSwitch = (Switch) findViewById(R.id.status_driver);
+        mWorkingSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                isloggingout = true;
-
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(DriversMapActivity.this, WelcomeActivity.class);
-                startActivity(intent);
-                finish();
-                return;
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    connectDriver();
+                } else {
+                    disconnectDriver();
+                }
             }
+
         });
     }
 
-                private void checkLocationPermission()  //after google banned the direct use of its api
-                 {
-                     if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-                     {
-                         if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)) {
-                             new AlertDialog.Builder(this)
-                                     .setTitle("Giver permission")
-                                     .setMessage("Give permission")
-                                     .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                                         @Override
-                                         public void onClick(DialogInterface dialog, int which) {
-                                             ActivityCompat.requestPermissions(DriversMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                                         }
-                                     })
-                                     .create()
-                                     .show();
-                         }
-                         else{
-                             ActivityCompat.requestPermissions(DriversMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                         }
-                     }
-                 }
 
-
-             @Override
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
         locationRequest = new LocationRequest();
         locationRequest.setInterval(1000);
         locationRequest.setFastestInterval(1000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); //best location<also drains a lot of battery>
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                mMap.setMyLocationEnabled(true);
+
             } else {
                 checkLocationPermission();
             }
-            mMap.getUiSettings().setZoomControlsEnabled(true);
-            mMap.setMinZoomPreference(11);
         }
-
-                    LocationCallback locationCallback= new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                    for(Location location : locationResult.getLocations()){
-                    if(getApplicationContext()!=null){
-
-                        if(!customerId.equals("") && lastLocation!=null && location != null){
-                            rideDistance += lastLocation.distanceTo(location)/1000;
-                        }
-                        lastLocation = location;
+    }
 
 
-                        LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-                        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            for (Location location : locationResult.getLocations()) {
+                if (getApplicationContext() != null) {
 
-                        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                        DatabaseReference refAvailable = FirebaseDatabase.getInstance().getReference("driversAvailable");
-                        DatabaseReference refWorking = FirebaseDatabase.getInstance().getReference("driversWorking");
-                        GeoFire geoFireAvailable = new GeoFire(refAvailable);
-                        GeoFire geoFireWorking = new GeoFire(refWorking);
+                    if (!customerId.equals("") && lastLocation != null && location != null) {
+                        rideDistance += lastLocation.distanceTo(location) / 1000;
+                    }
+                    lastLocation = location;
 
-                        switch (customerId){
-                            case "":
-                                geoFireWorking.removeLocation(userId);
-                                geoFireAvailable.setLocation(userId, new GeoLocation(location.getLatitude(), location.getLongitude()));
-                                break;
 
-                            default:
-                                geoFireAvailable.removeLocation(userId);
-                                geoFireWorking.setLocation(userId, new GeoLocation(location.getLatitude(), location.getLongitude()));
-                                break;
-                        }
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
+                    String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                    DatabaseReference refAvailable = FirebaseDatabase.getInstance().getReference("driversAvailable");
+                    DatabaseReference refWorking = FirebaseDatabase.getInstance().getReference("driversWorking");
+                    GeoFire geoFireAvailable = new GeoFire(refAvailable);
+                    GeoFire geoFireWorking = new GeoFire(refWorking);
+
+                    switch (customerId) {
+                        case "":
+                            geoFireWorking.removeLocation(userId);
+                            geoFireAvailable.setLocation(userId, new GeoLocation(location.getLatitude(), location.getLongitude()));
+                            break;
+
+                        default:
+                            geoFireAvailable.removeLocation(userId);
+                            geoFireWorking.setLocation(userId, new GeoLocation(location.getLatitude(), location.getLongitude()));
+                            break;
                     }
                 }
             }
-        };
+        }
+    };
 
-
-
-
-
-
-
-
-
-
-
-
-
-        //private void disconnectdriver ()
-        //{
-            //super.onStop();
-            //String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-            //DatabaseReference DriverAvailabilityRef = FirebaseDatabase.getInstance().getReference().child("Drivers Available");
-
-            //GeoFire geoFire = new GeoFire(DriverAvailabilityRef);
-            //geoFire.removeLocation(userID);
-
-        //}
-
-
-
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("give permission")
+                        .setMessage("give permission message")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(DriversMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(DriversMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
     }
-}
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        mFusedLocationClient.requestLocationUpdates(locationRequest, mLocationCallback, Looper.myLooper());
+                        mMap.setMyLocationEnabled(true);
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please provide the permission", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+
+
+    private void connectDriver() {
+        checkLocationPermission();
+        mFusedLocationClient.requestLocationUpdates(locationRequest, mLocationCallback, Looper.myLooper());
+        mMap.setMyLocationEnabled(true);
+    }
+
+    private void disconnectDriver() {
+        if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("driversAvailable");
+
+        GeoFire geoFire = new GeoFire(ref);
+        geoFire.removeLocation(userId);
+    }
+
+
+}
